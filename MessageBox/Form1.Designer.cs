a462c7c48c5481f5185e3516dac24517a961f65d
Özcan Kara <ozcankara810@gmail.com>
2023-03-19 15:58:57 +0300

    Initial commit

---
+++ MessageBox/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MessageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btonMessage = new System.Windows.Forms.Button();
+            this.btonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tboxMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btonMessage
+            // 
+            this.btonMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btonMessage.Location = new System.Drawing.Point(272, 177);
+            this.btonMessage.Name = "btonMessage";
+            this.btonMessage.Size = new System.Drawing.Size(208, 73);
+            this.btonMessage.TabIndex = 0;
+            this.btonMessage.Text = "Mesajı Göster";
+            this.btonMessage.UseVisualStyleBackColor = true;
+            this.btonMessage.Click += new System.EventHandler(this.btonMessage_Click);
+            // 
+            // btonExit
+            // 
+            this.btonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btonExit.Location = new System.Drawing.Point(673, 371);
+            this.btonExit.Name = "btonExit";
+            this.btonExit.Size = new System.Drawing.Size(115, 76);
+            this.btonExit.TabIndex = 1;
+            this.btonExit.Text = "Çıkış";
+            this.btonExit.UseVisualStyleBackColor = true;
+            this.btonExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mesaj :";
+            // 
+            // tboxMessage
+            // 
+            this.tboxMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxMessage.Location = new System.Drawing.Point(272, 110);
+            this.tboxMessage.Name = "tboxMessage";
+            this.tboxMessage.Size = new System.Drawing.Size(208, 38);
+            this.tboxMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tboxMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btonExit);
+            this.Controls.Add(this.btonMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btonMessage;
+        private System.Windows.Forms.Button btonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tboxMessage;
+    }
+}
+
---
+++ MessageBox/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btonMessage_Click(object sender, EventArgs e)
+        {
+            string message;
+
+            message = tboxMessage.Text;
+
+            //MessageBox.Show("Ali veli 49 50"); // aslında burada parametre göndermişim
+            MessageBox.Show("Ali veli 49 50");
+
+            //   MessageBox.Show(message); // aslında burada parametre göndermişim
+
+
+            tboxMessage.Text = "";
+            tboxMessage.Clear();
+        }
+    }
+}
